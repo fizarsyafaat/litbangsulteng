f088f1547c78f7dc6152630ebbbc977e6fe9c9f7
unknown <fizar.inf@gmail.com>
2022-07-22 09:45:00 +0800

    grafik pekerjaan

--- modules/Web/Admin/Adminpage/Controllers/FuncCont/JSON/PekerjaanJSON.php
+++ modules/Web/Admin/Adminpage/Controllers/FuncCont/JSON/PekerjaanJSON.php
@@ -10,6 +10,8 @@ use CodeIgniter\API\ResponseTrait;
 use BusinessProcessRoot\Models\MataPencaharianPokok as MataPencaharianPokokModel;
 use BusinessProcessRoot\Models\LabaPerBulan as LabaPerBulanModel;
 use BusinessProcessRoot\Models\Modal as ModalModel;
+use BusinessProcessRoot\Models\KkMain as KkMainModel;
+use BusinessProcessRoot\Models\Kelurahan as KelurahanModel;
 use BusinessProcessRoot\Models\KkMainDataPekerjaanDanOrganisasi as KkMainDataPekerjaanDanOrganisasiModel;
 
 
@@ -21,32 +23,26 @@ class PekerjaanJSON extends DefaultAdminFuncController{
 	}
 
 	public function json_get_pekerjaan(){
+		$request = $this->request;
+
 		$kModel = new MataPencaharianPokokModel();
-		$kcModel = new LabaPerBulanModel();
-		$ksModel = new ModalModel();
-		$kkmModel = new KkMainDataPekerjaanDanOrganisasiModel();
+		$klModel = new KkMainDataPekerjaanDanOrganisasiModel();
 
-		$k_list = $kModel->findAll();	
-		$kc_list = $kcModel->findAll();	
-		$ks_list = $ksModel->findAll();		
+		$kl_list = $klModel->findAll();
+		$k_array = [];
 
-		foreach($k_list as $m){
-			$m->total_data = sizeof($kkmModel->where("mata_pencaharian_pokok",$m->mata_pencaharian_pokok_id)->findAll());
-		}
-		foreach($kc_list as $m){
-			$m->total_data = sizeof($kkmModel->where("laba_per_bulan",$m->laba_per_bulan_id)->findAll());
-		}
-		foreach($ks_list as $m){
-			$m->total_data = sizeof($kkmModel->where("modal",$m->modal_id)->findAll());
+		foreach($kl_list as $kc){
+			$m = new \Stdclass();
+			$m->title = $kc->nama_mata_pencaharian_pokok;
+			$m->mata_pencaharian_pokok_id = $kc->mata_pencaharian_pokok_id;
+			$m->total_kk = sizeof($kModel->where("mata_pencaharian_pokok",$kc->mata_pencaharian_pokok_id)->findAll());
+			$k_array[] = $m;
 		}
 
-		$data = array(
-			'mata_pencaharian_pokok' => $k_list,
-			'laba_per_bulan' => $kc_list,
-			'modal' => $ks_list,
-		);
-		echo json_encode($data);
+		echo json_encode($k_array);
 	}
+
+			
 
 
 
--- public/assets/admin/custom/js/pekerjaan.js
+++ public/assets/admin/custom/js/pekerjaan.js
@@ -4,6 +4,137 @@ $(document).ready(function(){
 	get_pekerjaan();
 	get_modal();
 	get_laba();
+	kk_kelurahan();
+
+
+	
+	function kk_kelurahan(){
+		var page_csrf = $(".csrf-header-master").attr("name");
+		var page_csrf_value = $(".csrf-header-master").attr("value");
+
+		var data = {
+			[page_csrf] : page_csrf_value,
+		};
+
+		$.post(config_url + "panel/pekerjaan/json/get-pekerjaan",data,function(rd){
+			var bar_data = {
+				data : [],
+				bars : {show:true},
+			}
+
+			ticks_ar = [];
+
+			//sortir data
+			rd.sort(function(a, b) {
+			    return parseInt(b['total_kk']) - parseInt(a['total_kk']);
+			});
+
+			for(var i=0;i<10;i++){
+				ar = [];
+				tick = [];
+				ar.push(i,rd[i]['total_kk']);
+				tick.push(i,rd[i]['title']);
+
+				ticks_ar.push(tick);
+				bar_data.data.push(ar);
+			}
+
+			var total_kk_lainnya = 0;
+
+			for(var i=10;i<rd.length;i++){
+				total_kk_lainnya += rd[i]['total_kk']
+			}
+
+			ar = [];
+			tick = [];
+			ar.push(10,total_kk_lainnya);
+			tick.push(10,"Lainnya");
+
+			ticks_ar.push(tick);
+			bar_data.data.push(ar);
+
+
+		    $.plot("#bar-chart-kelurahan", [bar_data], {
+		        grid: {
+		            borderWidth: 1,
+		            borderColor: "#f3f3f3",
+		            tickColor: "#f3f3f3",
+		            hoverable: true,
+		            clickable: true,
+		        },
+		        series: {
+		            bars: {
+		                show: true,
+		                barWidth: 0.5,
+		                align: "center",
+		            },
+		        },
+	            valueLabels: {
+	                show: true
+	            },
+		        colors: ["#3c8dbc"],
+		        xaxis: {
+		            ticks: ticks_ar,
+		        },
+		    });
+
+		    //for ext
+		    var text = "";
+		    for(var is=0;is<rd.length;is++){
+			    text += "<tr><td>"+(parseInt(is)+1)+"</td><td>"+rd[is]['title']+"</td><td><a class='get-pekerjaan' href='#' data-id='"+rd[is]['mata_pencaharian_pokok_id']+"'>"+rd[is]['total_kk']+"</a></td></tr>";
+			}
+
+		    $("#jumlah-pekerjaan tbody").html(text);
+		},"json")
+		.fail(function(rd){
+			console.log(rd);
+		
+		});
+
+		var previousPoint = null,
+	    	previousLabel = null;
+
+		function showTooltip(x, y, color, contents) {
+		    $('<div id="tooltip">' + contents + '</div>').css({
+		        position: 'absolute',
+		        display: 'none',
+		        top: y - 40,
+		        left: x - 120,
+		        border: '2px solid ' + color,
+		        padding: '3px',
+		            'font-size': '12px',
+		            'border-radius': '5px',
+		            'background-color': '#fff',
+		            'font-family': 'Verdana, Arial, Helvetica, Tahoma, sans-serif',
+		        opacity: 0.9
+		    }).appendTo("body").fadeIn(200);
+		}
+
+		$("#bar-chart-kelurahan").on("plothover", function (event, pos, item) {
+		    if (item) {
+		        if ((previousLabel != item.series.label) || (previousPoint != item.dataIndex)) {
+		            previousPoint = item.dataIndex;
+		            previousLabel = item.series.label;
+		            $("#tooltip").remove();
+
+		            var x = item.datapoint[0];
+		            var y = item.datapoint[1];
+
+		            var color = item.series.color;
+
+		            //console.log(item.series.xaxis.ticks[x].label);               
+
+		            showTooltip(item.pageX,
+		            item.pageY,
+		            color,
+		                "<strong>Jumlah KK</strong><br>" + item.series.xaxis.ticks[x].label + " : <strong>" + y + "</strong>");
+		        }
+		    } else {
+		        $("#tooltip").remove();
+		        previousPoint = null;
+		    }
+		});
+	}
 
 	function get_pekerjaan(){
 		var id = 0;
@@ -54,9 +185,9 @@ $(document).ready(function(){
 	        });
 		},"json")
 		.fail(function(rd){
-			console.log(rd);
-		
+			console.log(rd);	
 		});
+
 	}
 
 	function get_laba(){
