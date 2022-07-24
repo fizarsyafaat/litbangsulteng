3935a85cfc485d3278fb2c666bb392868623f0b9
unknown <fizar.inf@gmail.com>
2022-07-24 08:34:55 +0800

    gasss fix

--- public/assets/admin/custom/js/pekerjaan.js
+++ public/assets/admin/custom/js/pekerjaan.js
@@ -7,7 +7,8 @@ $(document).ready(function(){
 	kk_kelurahan();
 
 
-	
+	var previousPoint = null,
+    	previousLabel = null;
 	
 	function get_pekerjaan(){
 		var id = 0;
@@ -62,6 +63,7 @@ $(document).ready(function(){
 		});
 
 	}
+	
 
 	function get_laba(){
 		var id = 0;
@@ -97,6 +99,8 @@ $(document).ready(function(){
 	                borderWidth: 1,
 	                borderColor: "#f3f3f3",
 	                tickColor: "#f3f3f3",
+		            hoverable: true,
+		            clickable: true,
 	            },
 	            series: {
 	                bars: {
@@ -104,6 +108,9 @@ $(document).ready(function(){
 	                    barWidth: 0.5,
 	                    align: "center",
 	                },
+	            },
+	            valueLabels: {
+	                show: true
 	            },
 	            colors: ["#3c8dbc"],
 		        xaxis: {
@@ -115,8 +122,49 @@ $(document).ready(function(){
 			console.log(rd);
 		
 		});
-	}
 
+		function showTooltip(x, y, color, contents) {
+		    $('<div id="tooltip">' + contents + '</div>').css({
+		        position: 'absolute',
+		        display: 'none',
+		        top: y - 40,
+		        left: x - 120,
+		        border: '2px solid ' + color,
+		        padding: '3px',
+		            'font-size': '12px',
+		            'border-radius': '5px',
+		            'background-color': '#fff',
+		            'font-family': 'Verdana, Arial, Helvetica, Tahoma, sans-serif',
+		        opacity: 0.9
+		    }).appendTo("body").fadeIn(200);
+		}
+		                
+		$("#bar-chart3").on("plothover", function (event, pos, item) {
+		    if (item) {
+		        if ((previousLabel != item.series.label) || (previousPoint != item.dataIndex)) {
+		            previousPoint = item.dataIndex;
+		            previousLabel = item.series.label;
+		            $("#tooltip").remove();
+
+		            var x = item.datapoint[0];
+		            var y = item.datapoint[1];
+
+		            var color = item.series.color;
+
+		            //console.log(item.series.xaxis.ticks[x].label);               
+
+		            showTooltip(item.pageX,
+		            item.pageY,
+		            color,
+		                "<strong>Jumlah KK</strong><br>" + item.series.xaxis.ticks[x].label + " : <strong>" + y + "</strong>");
+		        }
+		    } else {
+		        $("#tooltip").remove();
+		        previousPoint = null;
+		    }
+		});
+	}
+	
 	function get_modal(){
 		var id = 0;
 		var page_csrf = $(".csrf-header-master").attr("name");
@@ -151,6 +199,8 @@ $(document).ready(function(){
 	                borderWidth: 1,
 	                borderColor: "#f3f3f3",
 	                tickColor: "#f3f3f3",
+		            hoverable: true,
+		            clickable: true,
 	            },
 	            series: {
 	                bars: {
@@ -158,6 +208,9 @@ $(document).ready(function(){
 	                    barWidth: 0.5,
 	                    align: "center",
 	                },
+	            },
+	            valueLabels: {
+	                show: true
 	            },
 	            colors: ["#3c8dbc"],
 		        xaxis: {
@@ -169,7 +222,49 @@ $(document).ready(function(){
 			console.log(rd);
 		
 		});
+
+		function showTooltip(x, y, color, contents) {
+		    $('<div id="tooltip">' + contents + '</div>').css({
+		        position: 'absolute',
+		        display: 'none',
+		        top: y - 40,
+		        left: x - 120,
+		        border: '2px solid ' + color,
+		        padding: '3px',
+		            'font-size': '12px',
+		            'border-radius': '5px',
+		            'background-color': '#fff',
+		            'font-family': 'Verdana, Arial, Helvetica, Tahoma, sans-serif',
+		        opacity: 0.9
+		    }).appendTo("body").fadeIn(200);
+		}
+		                
+		$("#bar-chart2").on("plothover", function (event, pos, item) {
+		    if (item) {
+		        if ((previousLabel != item.series.label) || (previousPoint != item.dataIndex)) {
+		            previousPoint = item.dataIndex;
+		            previousLabel = item.series.label;
+		            $("#tooltip").remove();
+
+		            var x = item.datapoint[0];
+		            var y = item.datapoint[1];
+
+		            var color = item.series.color;
+
+		            //console.log(item.series.xaxis.ticks[x].label);               
+
+		            showTooltip(item.pageX,
+		            item.pageY,
+		            color,
+		                "<strong>Jumlah KK</strong><br>" + item.series.xaxis.ticks[x].label + " : <strong>" + y + "</strong>");
+		        }
+		    } else {
+		        $("#tooltip").remove();
+		        previousPoint = null;
+		    }
+		});
 	}
+
 	                
 	                /* END BAR CHART */
 });
